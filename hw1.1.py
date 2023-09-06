e75564e4e00225457004cd5b817c3ca77e2fba2b
htingmi <144210764+htingmi@users.noreply.github.com>
2023-09-06 16:20:47 +0800

    Add files via upload

---
+++ hw1.1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+height=float(input("請輸入您的身高(公尺):"))
+weight=float(input("請輸入您的體重(公斤):"))
+BMI=weight/(height**2)
+print(f"您的BMI是:{BMI:.2f}")
+
+
+# In[ ]:
+
+
+
+
